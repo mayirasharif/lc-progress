04e0da770c3185695c56604f960ba0c6979a146c
mhsharif <78449149+mayirasharif@users.noreply.github.com>
2026-03-07 13:13:16 -0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (66.41%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.py
@@ -0,0 +1,10 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        # what makes this process so hard? lol
+
+        actual_num1 = int(num1)
+        actual_num2 = int(num2)
+
+        actual_sum = actual_num1 * actual_num2
+
+        return str(actual_sum)
